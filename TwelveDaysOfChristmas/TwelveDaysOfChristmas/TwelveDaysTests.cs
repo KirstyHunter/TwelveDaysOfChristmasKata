74fa3351b66e97c69d7cdd6d09641828c4b92072
Kirsty Hunter <kirst2k@hotmail.com>
2015-12-14 21:27:42 +0000

    First Day of Christmas

---
+++ TwelveDaysOfChristmas/TwelveDaysOfChristmas/TwelveDaysTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwelveDaysOfChristmas
+{
+    [TestFixture]
+    public class TwelveDaysTests
+    {
+        [Test]
+        public void FirstDayOfChristmas_ShouldReturn_PartridgeInAPairTree()
+        {
+            var tweleveDays = new TwelveDaysofChristmas();
+            var song = tweleveDays.Sing(1);
+            Assert.AreEqual("a partridge in a pear tree", song);
+        }
+    }
+}
