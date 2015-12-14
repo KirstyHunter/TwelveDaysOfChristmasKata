2f679f057efcba9f0c938bf6b352a89a6a8262f8
Kirsty Hunter <kirst2k@hotmail.com>
2015-12-14 21:35:17 +0000

    Second day of christmas

--- TwelveDaysOfChristmas/TwelveDaysOfChristmas/TwelveDaysTests.cs
+++ TwelveDaysOfChristmas/TwelveDaysOfChristmas/TwelveDaysTests.cs
@@ -17,6 +17,14 @@ namespace TwelveDaysOfChristmas
             var song = tweleveDays.Sing(1);
             Assert.AreEqual("a partridge in a pear tree", song);
         }
+
+        [Test]
+        public void SecondDayOfChristmas_ShouldReturn_TwoTurtleDoves()
+        {
+            var tweleveDays = new TwelveDaysofChristmas();
+            var song = tweleveDays.Sing(2);
+            Assert.AreEqual("Two turtle doves", song);
+        }
     }
 
     public class TwelveDaysofChristmas
